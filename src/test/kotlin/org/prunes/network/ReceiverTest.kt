9be0fa7fa0cd4a7ae9d3ad8eac28c3c52ed942ce
mr.certain.a <mr.certain.a@gmail.com>
2022-02-11 17:45:33 +0900

    added files

---
+++ src/test/kotlin/org/prunes/network/ReceiverTest.kt
@@ -0,0 +1,49 @@
+package org.prunes.network
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.prunes.json.WideData
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import java.time.LocalDateTime
+
+internal class ReceiverTest: Spek({
+    describe("listen") {
+        context("json send") {
+            val port = 20202
+            Receiver(port).let {
+                it.listenJson { json ->
+                    println("received: $json")
+                }
+
+                val data = WideData().apply {
+                    registDate = LocalDateTime.now()
+                }
+
+                Transmitter.sendTo(port, data)
+
+                runBlocking { delay(1000) }
+                it.close()
+            }
+        }
+
+        context("send and receive") {
+            val port = 20202
+            Receiver(port).let {
+                it.listen { command ->
+                    println("received: $command")
+                }
+
+                repeat(5) { i->
+                    Transmitter.sendTo(port, "test $i")
+                }
+
+                runBlocking { delay(1000) }
+                it.close()
+
+                val res = Transmitter.sendTo(port, "error")
+                println("送信結果:$res")
+            }
+        }
+    }
+})
